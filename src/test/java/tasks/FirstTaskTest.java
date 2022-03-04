4326cabcf658ced07c977b424962201c4c405e93
Deniz Onursal <onursaldeniz@protonmail.com>
2022-03-04 17:17:10 +0300

    Adding test for first task.

--- src/test/java/tasks/FirstTaskTest.java
+++ src/test/java/tasks/FirstTaskTest.java
@@ -1,7 +1,22 @@
 package tasks;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
-class Task1Test {
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static tasks.Task1.solution;
+
+class FirstTaskTest {
+
+    @Test
+    void testAgainstGivenCase() {
+        final var helloWold = "Hello World!";
+        final var length = solution(helloWold);
+
+        assertEquals(6, length);
+    }
 
 }
